a6cc5efe8b4ed96db496564bfa0a4133272fed29
Vladislaw <kumiho2708@gmail.com>
2025-10-26 20:08:43 +0200

    Update

--- lib/api.ts
+++ lib/api.ts
@@ -20,9 +20,6 @@ export interface NewNote {
 
 const api = axios.create({
   baseURL: 'https://notehub-public.goit.study/api',
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
-  },
 })
 
 // export const fetchNotes = async ({
